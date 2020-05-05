61f38e50c666f94d0f5d313c4aea2e728c6ee960
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-05-05 19:30:07 +0200

    create find method for Text class

--- nlp/text.py
+++ nlp/text.py
@@ -3,16 +3,55 @@ import re
 
 
 class Text:
+    """
+    Text manipulation class.
+    
+    Parameters:
+    -----------
+        text: string
+            Text to be manipulated.
+    """
     def __init__(self, text: str) -> None:
         self.body = re.split("\s+", text)
         self.text = text
 
-    def concordance(self, word: str, size: int, ret: bool = 0) -> List[str]:
+    def find(self, word: str, n: int = None) -> List[int]:
+        """
+        Returns indexes (or first n indexes if n is specified) of a given word in text.
+
+        Parameters:
+        -----------
+            word: string
+                Word that will be searched in the text. 
+            n: int
+                Number of indexes that will be returned.
+
+        """
         idxs = [index for index, w in enumerate(self.body) if w in [word,
                                                                     word+'.',
                                                                     word+',']]
+        if n is not None:
+            return idxs[:n]
+        
+        return idxs
+
+    def concordance(self, word: str, size: int, ret: bool = 0) -> List[str]:
+        """"
+        Returns all occurrences of a given word with given amount of additionals words from left and right side.
+
+        Parameters:
+        -----------
+            word: string
+                Word that will be searched in the text.
+            size: int
+                Number of strings to be added to final string from left and right side of given word.
+
+        """
+
+        idxs = self.find(word)
         
         occurs = [self.body[index - size:index] + self.body[index:index + size + 1] for index in idxs]
+        
         if ret:
             return occurs
         else:
@@ -21,6 +60,5 @@ class Text:
 
 
 t = Text("My name is Alex. Hope you have a good day. Alex, Alex")
-t.concordance("Alex", 3)
-
+t.concordance("Alex", 2)
 
