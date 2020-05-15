c0618eea19982d612d8fbabda77e667ef4d48605
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-05-15 14:01:31 +0200

    add word_tokenize to Tokenizer class

--- nlp/preprocessing.py
+++ nlp/preprocessing.py
@@ -1,3 +1,19 @@
+import re
+
+
 class Tokenizer:
     def __init__(self):
-        pass
+        self.tokens = []
+        self.word2idx = {}
+
+    def word_tokenize(self, sent: str):
+        s = re.sub('([.,!?()#])', r' \1 ', sent)
+        s = re.sub('\s{2,}', ' ', s)
+        self.tokens = re.split("\s+", s)
+        return self.tokens
+    
+
+t = Tokenizer()
+tokens = t.word_tokenize("I am stupid. #Bro, come on")
+print(tokens)
+            
