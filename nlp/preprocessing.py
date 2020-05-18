4f0bae4aba06d498607e99c88982b0105ffae820
Piotrekszmel <piotr.szmelczynski@gmail.com>
2020-05-18 14:00:05 +0200

    add create_word2idx method to Tokenizer class

--- nlp/preprocessing.py
+++ nlp/preprocessing.py
@@ -1,3 +1,4 @@
+from typing import List
 import re
 
 
@@ -6,14 +7,23 @@ class Tokenizer:
         self.tokens = []
         self.word2idx = {}
 
-    def word_tokenize(self, sent: str):
+    def word_tokenize(self, sent: str) -> List:
         s = re.sub('([.,!?()#])', r' \1 ', sent)
         s = re.sub('\s{2,}', ' ', s)
         self.tokens = re.split("\s+", s)
         return self.tokens
     
+    def create_word2idx(self):
+        assert len(self.tokens) > 0, "First create tokens"
+        self.word2idx = {k: v for (k, v) in enumerate(set(self.tokens))}
+        return self.word2idx
+
+    def fit_on_text(text: str):
+        pass
 
 t = Tokenizer()
-tokens = t.word_tokenize("I am stupid. #Bro, come on")
+tokens = t.word_tokenize("Hello man I am cool and cool and you")
+word2idx = t.create_word2idx()
 print(tokens)
-            
+print("\n")
+print(word2idx) 
